ecc2566d36977b565ab7cc35a18a65c5217b08f8
ZeelPatel <zeelsakariya07@gmail.com>
2026-02-18 15:53:51 -0600

    Completed Ruby exercise for Jaguar Design Studio - Web Developer Role

--- exercise.rb
+++ exercise.rb
@@ -5,6 +5,24 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+    str.split.map do |word|
+      # Separate word body and punctuation (e.g., "today?")
+      match = word.match(/^([A-Za-z]+)([^A-Za-z]*)$/)
+
+      if match
+        text = match[1]
+        punctuation = match[2]
+
+        if text.length > 4
+          replacement = text[0] == text[0].upcase ? "Marklar" : "marklar"
+          replacement + punctuation
+        else
+          word
+        end
+      else
+        word
+      end
+    end.join(" ")
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +31,19 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+    return 0 if nth < 3
+
+    a, b = 1, 1
+    sum = 0
+
+    (3..nth).each do
+      c = a + b
+      sum += c if c.even?
+      a = b
+      b = c
+    end
+
+    sum
   end
 
 end
